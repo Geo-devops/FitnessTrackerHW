73636a53e45442ec30f8c03dd58f0a3241196eb2
GBondarek <georgettebondarek@gmail.com>
2021-02-04 20:09:08 -0500

    index.js

--- models/index.js
+++ models/index.js
@@ -1,3 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema; 
+
+const workoutSchema = new Schema({
+    name: {
+        type: String, 
+        unique: true,
+        required: true
+
+    },
+    exercises: [{
+        type: Schema.Types.ObjectId,
+        ref: "Exercise"
+    }]
+});
+
+const exercisesSchema = new Schema
+
+
+
+
+
+
+
+
+
 module.exports = {
     Workout: require("./workout")
 }
--- server.js
+++ server.js
@@ -18,6 +18,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useUnifiedTopology: true,
 });
 
+var routes = require("./controller/fitness_controller.js");
+
 // routes
 // app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
